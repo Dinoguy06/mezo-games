82f4b39abadebde1b3aecad788c315cb27733e9a
Dinoguy06 <102428171+Dinoguy06@users.noreply.github.com>
2022-10-08 14:53:33 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,59 @@
-var box;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase, playerArcher;
+var baseimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
 function setup() {
-  createCanvas(400,400);
-  box = createSprite(200,200, 50, 50);
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+
+ // playerArcher = new ( 340, playerBase.position.y - 112, 120, 120);
+  playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
+ // playerArcher =  PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
+ // playerArcher = new PlayerArcher( );
+
 }
 
-function draw() 
-{
-  background(30);
-  if(keyIsDown(RIGHT_ARROW))
-  {
-    box.position.x=box.position.x+5
-  }
-  if(keyIsDown(LEFT_ARROW))
-  {
-    box.position.x=box.position.x-5
-  }
-  if(keyIsDown(UP_ARROW))
-  {
-    box.position.y=box.position.y-5
-  }
-  if(keyIsDown(DOWN_ARROW))
-  {
-    box.position.y=box.position.y+5
-  }
-  drawSprites()
+function draw() {
+  background(backgroundImg);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+
+  Engine.update(engine);
+
+
+  // playerArcher.display;
+  // playerArcherdisplay();
+  playerArcher.display();
+  // display();
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
 }
-
-
-
-
